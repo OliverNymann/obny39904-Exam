fca882ec57f70344382d475f74ce826f1441afdc
OliverNymann <55433479+OliverNymann@users.noreply.github.com>
2021-12-17 11:27:08 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -5,22 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="checkOutStyle.css">
-    <title>Document</title>
+    <title>Check ud</title>
 </head>
 <body>
     <div class="form">
         <form class="checkOut" action="orderConfirmation.php" method="POST">
-            Fornavn: <input type="text" name="firstname">
+            Fornavn: <input type="text" name="firstname" required>
             <br>
-            Efternavn: <input type="text" name="lastname">
+            Efternavn: <input type="text" name="lastname" required>
             <br>
-            Adresse: <input type="text" name="adress">
+            Adresse: <input type="text" name="adress" required>
             <br>
-            Postnummer: <input type="text" name="zipCode">
+            Postnummer: <input type="text" name="zipCode" required>
             <br>
-            Email: <input type="text" name="email">
+            Email: <input type="text" name="email" required>
             <br>
-            <input type="submit" value="Gå til betaling">
+            <input type="submit" value="Gå til betaling" required>
+            
         </form>
     </div>
 </body>
